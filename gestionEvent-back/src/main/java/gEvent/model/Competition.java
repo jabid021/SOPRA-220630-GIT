5a8c82388fed9fd5b1fd913c2cdf655cf1c44d1a
Filixo <alaeboughaba@gmail.com>
2022-07-28 10:25:14 +0200

    Imports Id

--- gestionEvent-back/src/main/java/gEvent/model/Competition.java
+++ gestionEvent-back/src/main/java/gEvent/model/Competition.java
@@ -9,9 +9,7 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
 import javax.persistence.OneToMany;
 @Entity
 @DiscriminatorValue("competition")

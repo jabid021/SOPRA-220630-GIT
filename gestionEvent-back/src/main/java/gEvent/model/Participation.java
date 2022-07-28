cde0604dbf8592506501e6247c401d3d1ab3d6dd
Louis <louischatelier21@gmail.com>
2022-07-28 10:05:40 +0200

    modif participation + création DAO +modif singleton

--- gestionEvent-back/src/main/java/gEvent/model/Participation.java
+++ gestionEvent-back/src/main/java/gEvent/model/Participation.java
@@ -1,17 +1,62 @@
 package gEvent.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Participation {
 
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
+	@OneToMany
 	private User user;
+
+	@OneToMany
 	private Competition competion;
-	
-	
+
+	public Participation() {
+
+	}
+
 	public Participation(User user, Competition competion) {
 		this.user = user;
 		this.competion = competion;
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public Competition getCompetion() {
+		return competion;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public void setCompetion(Competition competion) {
+		this.competion = competion;
+	}
+
+	@Override
+	public String toString() {
+		return "Participation [id=" + id + ", user=" + user + ", competion=" + competion + "]";
+	}
+
 	
 	
 	

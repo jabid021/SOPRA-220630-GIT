20b15baeaa97ae37037b7e73a6abff7de3ebddb7
Auriane97 <auriane.francois@gmail.com>
2022-07-28 10:31:19 +0200

    Intervenant+daoIntervenant

--- gestionEvent-back/src/main/java/gEvent/model/Intervenant.java
+++ gestionEvent-back/src/main/java/gEvent/model/Intervenant.java
@@ -1,16 +1,28 @@
 package gEvent.model;
 
 import java.util.ArrayList;
+
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Intervenant {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nom ;
 	
 	
 	private List<Talent> talents = new ArrayList();
 
+	public Intervenant() {}
+	
 	public Intervenant(String nom) {
 		this.nom = nom;
 	}
@@ -41,6 +53,11 @@ public class Intervenant {
 
 	public void setId(Integer id) {
 		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Intervenant [id=" + id + ", nom=" + nom + "]";
 	}
 
 

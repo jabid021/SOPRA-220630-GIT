1f49740d7801946276130b31d76f9001e51b85fd
AriBad <ariane.badoual@gmail.com>
2022-07-28 10:12:17 +0200

    mapping competition

--- gestionEvent-back/src/main/java/gEvent/model/Competition.java
+++ gestionEvent-back/src/main/java/gEvent/model/Competition.java
@@ -2,23 +2,51 @@ package gEvent.model;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+@Entity
+@DiscriminatorValue("competition")
 public class Competition extends Evenement {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(columnDefinition = "INT(11)")
 	private User gagnant;
 	
-	//mappedBy
+	@OneToMany(mappedBy = "competition")
 	private List<Participation> participants;
 	
+	@Enumerated(EnumType.STRING)
+	@Column(columnDefinition = "ENUM('Judo', 'Foot', 'LoL', 'Ski', 'BierrePong')")
 	private Sport sport;
 	
+	public Competition() {
+		// TODO Auto-generated constructor stub
+	}
 	
 	public Competition(String libelle, LocalDate dateDebut,  LocalTime heureDebut,LocalDate dateFin, LocalTime heureFin,
 			double prix, Sport sport, Adresse adresse) {
 		super(libelle,dateDebut,dateFin,heureDebut,heureFin,prix,adresse);
 		this.sport = sport;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
 	}
 
 	public User getGagnant() {

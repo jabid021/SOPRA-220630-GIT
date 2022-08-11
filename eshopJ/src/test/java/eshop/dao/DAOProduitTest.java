e30e9fac4ce85cb113c5e59d60d793b164d6070a
Jordan <Jordanabid@gmail.com>
2022-08-11 13:11:41 +0200

    Junit DONE

--- eshopJ/src/test/java/eshop/dao/DAOProduitTest.java
+++ eshopJ/src/test/java/eshop/dao/DAOProduitTest.java
@@ -1,25 +1,99 @@
 package eshop.dao;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import javax.transaction.Transactional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import eshop.config.AppConfig;
+import eshop.model.Fournisseur;
+import eshop.model.Produit;
+
+@RunWith(SpringJUnit4ClassRunner.class) 
+@ContextConfiguration(classes={ AppConfig.class})
+
+@Transactional
 public class DAOProduitTest {
 
-	
-	
-	public void initDAO() {}
-	
 
+	@Autowired
+	IDAOProduit daoProduit;
+
+	@Autowired
+	IDAOPersonne daoPersonne;
+
+	@Test
+	public void initDAO() 
+	{
+		assertNotNull(daoProduit);
+	}
+
+
+	@Test
 	public void addTest() 
 	{
-		
+
+		//Arrange
+		Fournisseur f = new Fournisseur("nom", "prenom", "societe");
+		f=(Fournisseur) daoPersonne.save(f);
+		Produit p = new Produit("lib", 1, f);
+		Produit p2;
+		//Act
+		p=daoProduit.save(p);
+		p2 = daoProduit.findById(p.getId());
+		//Assert
+		if(p2==null) {fail();}
+		else 
+		{
+			assertEquals("lib", p2.getLibelle());
+			assertTrue(1==p2.getPrix());
+			assertEquals(f.getId(), p2.getFournisseur().getId());
+
+		}
+
 	}
-	
+
+	@Test
 	public void findTest() 
 	{
-		
+		//Arrange
+		Fournisseur f = new Fournisseur("nom", "prenom", "societe");
+		f=(Fournisseur) daoPersonne.save(f);
+		Produit p = new Produit("lib", 1, f);
+		p=daoProduit.save(p);
+		Produit p2;
+		//Act
+		p2 = daoProduit.findById(p.getId());
+		//Assert
+		if(p2==null) {fail();}
+
 	}
-	
-	
+
+	@Test
 	public void deleteTest() 
 	{
-		
+		//Arrange
+		Fournisseur f = new Fournisseur("nom", "prenom", "societe");
+		f=(Fournisseur) daoPersonne.save(f);
+		Produit p = new Produit("lib", 1, f);
+		p=daoProduit.save(p);
+		Produit p2;
+		//Act
+
+		daoProduit.delete(p.getId());
+		p2=daoProduit.findById(p.getId());
+		//Assert
+		assertNull(p2);
+
+
 	}
 }
--- orchestre-Spring/src/test/java/orchestre/dao/DAOPersonneTest.java
+++ orchestre-Spring/src/test/java/orchestre/dao/DAOPersonneTest.java
@@ -8,7 +8,7 @@ import javax.transaction.Transactional;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.Rollback;
+import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -19,11 +19,13 @@ import orchestre.model.Personne;
 @ContextConfiguration(classes={ AppConfig.class})
 
 @Transactional
-@Rollback(true)
 public class DAOPersonneTest {
 
 	@Autowired
 	IDAOPersonne daoPersonne;
+	
+	
+	
 	
 	@Test
 	public  void demoInsert() 

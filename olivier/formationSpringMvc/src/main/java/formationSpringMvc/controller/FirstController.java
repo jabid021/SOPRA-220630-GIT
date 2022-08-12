936f57046332edc7c4655f75c271ad4b789142f2
olivier <o.gozlan@ajc-ingenierie.fr>
2022-08-12 17:07:23 +0200

    requestparam

--- olivier/formationSpringMvc/src/main/java/formationSpringMvc/controller/FirstController.java
+++ olivier/formationSpringMvc/src/main/java/formationSpringMvc/controller/FirstController.java
@@ -1,13 +1,26 @@
 package formationSpringMvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/demo")
 public class FirstController {
 
-	@RequestMapping("/hello")
-	public String hello() {
+	// @RequestMapping("/hello")
+	@GetMapping("/hello")
+	public String hello(Model model, 
+			@RequestParam String nom) {
+		model.addAttribute("nom", nom);
 		return "hello";
+	}
+
+	@RequestMapping(path = "/bye", method = RequestMethod.GET)
+	public String bye() {
+		return "bye";
 	}
 }
--- olivier/formationSpringMvc/src/main/webapp/WEB-INF/views/bye.jsp
+++ olivier/formationSpringMvc/src/main/webapp/WEB-INF/views/bye.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<h1>hello ${nom}</h1>
+	<h1>bye</h1>
 </body>
 </html>

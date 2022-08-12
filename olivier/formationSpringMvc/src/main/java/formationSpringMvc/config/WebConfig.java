096fff7750211e3915fc622773bc1a1fc4a4be4f
olivier <o.gozlan@ajc-ingenierie.fr>
2022-08-12 15:24:47 +0200

    creation projet mvc

---
+++ olivier/formationSpringMvc/src/main/java/formationSpringMvc/config/WebConfig.java
@@ -0,0 +1,29 @@
+package formationSpringMvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("formationSpringMvc.controller")
+public class WebConfig implements WebMvcConfigurer {
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+	}
+
+	@Bean
+	public UrlBasedViewResolver viewResolver() {
+		UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		viewResolver.setViewClass(JstlView.class);
+		return viewResolver;
+	}
+}
